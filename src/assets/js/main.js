2ea3b04b3b1562a27eef4afb6fad81ffd978e6d2
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-05-17 13:41:24 +0900

    Update top design

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,21 @@
-$( '#username' ).keypress( function ( e ) {
+$('#username').keypress( function ( e ) {
   if (e.which === 13) {
-    $.ajax({
-      url:'https://api.github.com/users/watilde/starred',
-      data:{
-        direction: 'asc',
-      }
-    }).done(function (data) {
-      var first_one = data.shift();
-      console.log(first_one);
-      $('#main').html(JSON.stringify(first_one, null, '\t'));
-    });
+    search();
   }
 });
+
+$('#search').on('click', function (){
+  search();
+});
+
+function search () {
+  $.ajax({
+    url:'https://api.github.com/users/watilde/starred',
+    data:{
+      direction: 'asc',
+    }
+  }).done(function (data) {
+    var first_one = data.shift();
+    $('#main').html(JSON.stringify(first_one, null, '\t'));
+  });
+}
